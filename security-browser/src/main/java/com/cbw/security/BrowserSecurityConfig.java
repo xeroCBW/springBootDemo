27fa6b62f59a014e79502a5e439d0fd16b855b9c
DESKTOP-I827G3G\cbw <861754186@qq.com>
2018-06-12 20:32:47 +0800

    初步完成认证弹框

---
+++ security-browser/src/main/java/com/cbw/security/BrowserSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.cbw.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+/**
+ * 设置认证拦截
+ * @author cbw
+ *
+ */
+@Component
+public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//设置认证
+		
+		http.formLogin()
+		.and()
+		.authorizeRequests()
+		.anyRequest()
+		.authenticated();
+	}
+	
+	
+}
