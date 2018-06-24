f8299a043723b0b40d183f9acfb18e44fcd602ab
DESKTOP-I827G3G\cbw <861754186@qq.com>
2018-06-24 15:30:34 +0800

    引入登入文件夹

--- Demo/src/main/java/com/cbw/ErrorPageConfig.java
+++ Demo/src/main/java/com/cbw/ErrorPageConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 
-@Configuration
+//@Configuration
 public class ErrorPageConfig {
 	
 	/**
